62effa7ec211ca351f8deef08b414c8ed4ca95b1
Surendra-moond <surendramoond9216@gmail.com>
2025-08-08 23:20:52 +0530

    datatype

---
+++ javascript/first.js
@@ -0,0 +1,11 @@
+// how to create objects in js
+// const surendra={
+//     fullName:"surendra moond",
+//     age : 20,
+//     fatherName:"rameshwar lal",
+//     cgpa:8,
+// }
+const fullName="surendra moond";
+var eMail="suruchoudhary";
+let id =24115093;
+console.table([fullName,eMail,id]);
